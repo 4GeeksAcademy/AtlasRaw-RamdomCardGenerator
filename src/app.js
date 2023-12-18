28e573f3b67432e5f155105bfb50e74471fe864d
atlasraw <144444822+atlasraw@users.noreply.github.com>
2023-12-18 19:37:05 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  getRandomCard();
 };
+
+function getRandomCard() {
+  let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numIndex = Math.floor(Math.random() * num.length);
+  let suitsIndex = Math.floor(Math.random() * suits.length);
+
+  const cardDiv = document.querySelector(".card");
+  cardDiv.classList = "card " + suits[suitsIndex];
+
+  const suitElement = suits[suitsIndex];
+  const numElement = num[numIndex];
+
+  const header = cardDiv.querySelector(".header-card");
+  const footer = cardDiv.querySelector(".footer-card");
+  const value = cardDiv.querySelector(".num-card");
+
+  header.textContent = suitElement;
+  footer.textContent = suitElement;
+  value.textContent = numElement;
+}
+const button = document.querySelector("button");
+button.addEventListener("click", function() {
+  getRandomCard();
+});
+
+function tenSecondTimer() {
+  getRandomCard();
+}
+setInterval(tenSecondTimer, 10000);
